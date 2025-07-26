83235e3a626a37455c1f4b4720fc4124f39aeed2
Andrian prayugo <andrianprayugo74@gmail.com>
2025-07-26 15:16:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import math
 st.markdown("""
     <style>
     .stApp {
-        background-image: linear-gradient(rgba(0,0,0,0.90), rgba(0,0,0,0.90)),
+        background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),
                           url("https://i.imgur.com/BSBUvyu.jpeg");
         background-size: cover;
         background-attachment: fixed;
